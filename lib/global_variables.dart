c3183ff09e58bcf421fcd85eac1700b2d6d9116e
Anay Dwivedi <anaydwivedi25@gmail.com>
2024-01-08 22:53:33 +0530

    this os online shoe shopping app project

---
+++ lib/global_variables.dart
@@ -0,0 +1,34 @@
+final products = [
+  {
+    'id': '0',
+    'title': 'Men\'s Nike Shoe',
+    'price': 44.52,
+    'imageUrl': 'lib/assets/images/shoes_3.png',  
+    'company': 'Nike',
+    'sizes': [8, 9, 10, 11],
+  },
+  {
+    'id': '1',
+    'title': 'Adidas Shoe',
+    'price': 20.12,
+    'imageUrl': 'lib/assets/images/shoes_2.png',
+    'company': 'Adidas',
+    'sizes': [9, 10, 11],
+  },
+  {
+    'id': '2',
+    'title': 'Jordan Shoes',
+    'price': 44.52,
+    'imageUrl': 'lib/assets/images/shoes_4.png',
+    'company': 'Nike',
+    'sizes': [8, 9, 10, 11],
+  },
+  {
+    'id': '3',
+    'title': 'Bata Women\'s Shoe',
+    'price': 30.23,
+    'imageUrl': 'lib/assets/images/shoes_1.png',
+    'company': 'Bata',
+    'sizes': [8, 9, 10],
+  },
+];
